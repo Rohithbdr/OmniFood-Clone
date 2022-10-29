47246611294de36f74791b0dbb9bfed7ee0fce37
Rohithbdr <34566736+Rohithbdr@users.noreply.github.com>
2022-10-30 01:39:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 let navbar = document.getElementById("navbar");
 let ham = document.getElementById("ham");
+let navItem  = document.querySelectorAll('.nav-link');
+let header = document.querySelector('header');
 
 
 ham.onclick = () => {
     navbar.classList.toggle("active");
-    ham.classList.toggle("cross")
+    ham.classList.toggle("cross");
 }
+
+navItem.forEach((event) => {
+    event.onclick = () => {
+        navbar.classList.remove("active");
+        ham.classList.remove("cross");
+    }
+} )
+
+window.addEventListener('scroll' , (event) => {
+    if(window.scrollY > 760) {
+        header.style.backgroundColor = "#fff";
+        header.style.position = "fixed";
+    } else {
+        header.style.backgroundColor = "#fdf2e9";
+        header.style.position = "static";
+    }
+})
+
